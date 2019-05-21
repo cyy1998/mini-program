959f0e8958fa2bf810cc27ab566b5e21e8f31c60
cyy1998 <caiyiyang1998@126.com>
2019-05-21 19:42:14 +0800

    chooseDate--origin

--- pages/diary/diary.js
+++ pages/diary/diary.js
@@ -1,36 +1,117 @@
-const date = new Date()
-const years = []
-const months = []
-const days = []
-
-for (let i = 1990; i <= date.getFullYear(); i++) {
-  years.push(i)
-}
-
-for (let i = 1; i <= 12; i++) {
-  months.push(i)
-}
-
-for (let i = 1; i <= 31; i++) {
-  days.push(i)
-}
-
+const app=getApp()
 Page({
   data: {
-    years,
-    year: date.getFullYear(),
-    months,
-    month: 2,
-    days,
-    day: 2,
-    value: [9999, 1, 1],
+    year: 0,
+    month: 0,
+    day: 0,
+    date: ['日', '一', '二', '三', '四', '五', '六'],
+    dateArr: [],
+    isToday: 0,
+    isTodayWeek: false,
+    todayIndex: 0,
+    cal_show: true
   },
-  bindChange(e) {
-    const val = e.detail.value
+  onLoad: function () {
+    let now = new Date();
+    let year = now.getFullYear();
+    let month = now.getMonth() + 1;
+    this.dateInit();
     this.setData({
-      year: this.data.years[val[0]],
-      month: this.data.months[val[1]],
-      day: this.data.days[val[2]]
+      year: year,
+      month: month,
+      day: now.getDate(),
+      isToday: '' + year + month + now.getDate()
+    })
+  },
+  dateInit: function (setYear, setMonth) {
+    //全部时间的月份都是按0~11基准，显示月份才+1
+    let dateArr = [];						//需要遍历的日历数组数据
+    let arrLen = 0;							//dateArr的数组长度
+    let now = setYear ? new Date(setYear, setMonth) : new Date();
+    let year = setYear || now.getFullYear();
+    let nextYear = 0;
+    let month = setMonth || now.getMonth();					//没有+1方便后面计算当月总天数
+    let nextMonth = (month + 1) > 11 ? 1 : (month + 1);
+    let startWeek = new Date(year + ',' + (month + 1) + ',' + 1).getDay();							//目标月1号对应的星期
+    let dayNums = new Date(year, nextMonth, 0).getDate();				//获取目标月有多少天
+    let obj = {};
+    let num = 0;
+
+    if (month + 1 > 11) {
+      nextYear = year + 1;
+      dayNums = new Date(nextYear, nextMonth, 0).getDate();
+    }
+    arrLen = startWeek + dayNums;
+    for (let i = 0; i < arrLen; i++) {
+      if (i >= startWeek) {
+        num = i - startWeek + 1;
+        obj = {
+          isToday: '' + year + (month + 1) + num,
+          dateNum: num,
+          weight: 5
+        }
+      } else {
+        obj = {};
+      }
+      dateArr[i] = obj;
+    }
+    this.setData({
+      dateArr: dateArr
+    })
+
+    let nowDate = new Date();
+    let nowYear = nowDate.getFullYear();
+    let nowMonth = nowDate.getMonth() + 1;
+    let nowWeek = nowDate.getDay();
+    let getYear = setYear || nowYear;
+    let getMonth = setMonth >= 0 ? (setMonth + 1) : nowMonth;
+
+    if (nowYear == getYear && nowMonth == getMonth) {
+      this.setData({
+        isTodayWeek: true,
+        todayIndex: nowWeek
+      })
+    } else {
+      this.setData({
+        isTodayWeek: false,
+        todayIndex: -1
+      })
+    }
+  },
+  lastMonth: function () {
+    //全部时间的月份都是按0~11基准，显示月份才+1
+    let year = this.data.month - 2 < 0 ? this.data.year - 1 : this.data.year;
+    let month = this.data.month - 2 < 0 ? 11 : this.data.month - 2;
+    this.setData({
+      year: year,
+      month: (month + 1)
+    })
+    this.dateInit(year, month);
+  },
+  nextMonth: function () {
+    //全部时间的月份都是按0~11基准，显示月份才+1
+    let year = this.data.month > 11 ? this.data.year + 1 : this.data.year;
+    let month = this.data.month > 11 ? 0 : this.data.month;
+    this.setData({
+      year: year,
+      month: (month + 1)
+    })
+    this.dateInit(year, month);
+  },
+  selectDay: function(e)
+  {
+    let today=e.target.dataset.text;
+    let year=this.data.year;
+    let month=this.data.month;
+    this.setData({
+      day: today,
+      isToday: '' + year + month + today
+      })
+  },
+  showCal: function()
+  {
+    this.setData({
+      cal_show: this.data.cal_show? false: true
     })
   }
 })
